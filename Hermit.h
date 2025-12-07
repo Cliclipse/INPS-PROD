6d28efe6059da32cd39a0a98447deebaaf3abb27
Ewen Expuesto <ewen.expuesto@gmail.com>
2025-12-07 04:34:15 +0100

    Doxygen comments

--- Hermit.h
+++ Hermit.h
@@ -3,6 +3,9 @@
 
 #include "armadillo-code-15.0.x/armadillo-code-15.0.x/include/armadillo"
 
+/**
+ * @brief Helper for generating Hermite polynomial matrices.
+ */
 class Hermit
 {
 public:
@@ -12,10 +15,21 @@ public:
     arma::rowvec zValues;   
     arma::mat matrixValues; 
 
+    /**
+     * @brief Construct the Hermite table for the provided abscissas.
+     * @param ordreN highest polynomial order to compute.
+     * @param zValues_ row vector containing the sampling points.
+     */
     Hermit(int ordreN, const arma::rowvec& zValues_);
 
+    /**
+     * @brief Populate matrixValues with Hermite evaluations up to ordreMax.
+     */
     void CalcMatrix();
 
+    /**
+     * @brief Return the final Hermite row (order ordreMax).
+     */
     arma::rowvec GetHermit();
 };
 

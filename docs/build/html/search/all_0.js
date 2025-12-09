144465e3e0c4d2c0b7b6beb6a3055609e9d56ded
Ewen Expuesto <ewen.expuesto@gmail.com>
2025-12-09 01:50:09 +0100

    pres

--- docs/build/html/search/all_0.js
+++ docs/build/html/search/all_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['basis_0',['basis',['../classBasis.html',1,'Basis'],['../classBasis.html#a79a154fabf5f316a85fa7ab9038d2fb0',1,'Basis::Basis()']]]
+  ['basis_0',['basis',['../classBasis.html',1,'Basis'],['../classBasis.html#a79a154fabf5f316a85fa7ab9038d2fb0',1,'Basis::Basis(double br, double bz, int N, double Q)']]],
+  ['basisfunc_1',['basisFunc',['../classBasis.html#a76e3a2962a9bd5b8596c4d1d1795cfe6',1,'Basis']]]
 ];
